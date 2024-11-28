27acac8e06395fea87328689bf5a5a96db469b10
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-28 11:51:05 +0900

    update ok

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -29,14 +29,13 @@ const getTasks = async (req,res) => {
     }
 }
 
-const updateTasks = async (req,res) => {
+const updateTasks =  (req,res) => {
     try{
-        const {id: taskID} = req.params
-        const task = await Task.findOneAndUpdate({_id: taskID});
-        if(!task){
-            return res.status(404).json({msg: `No task with id : ${taskID}`})
-        }
-        res.status(200).json({task})
+        const {id:taskID} = req.params
+        res.status(200).json({id:taskID, data:req.body})
+        
+
+        
     }catch(error){
         res.status(500).json({msg: error})
     }
--- routes/tasks.js
+++ routes/tasks.js
@@ -9,8 +9,8 @@ router.route('/')
 
 router.route('/:id')
     .get(getTasks)
-    .post(deleteTasks)
-    .post(updateTasks)
+    .delete(deleteTasks)
+    .patch(updateTasks)
 
 
 
