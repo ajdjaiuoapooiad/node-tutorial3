29cb1149b9c8e06ec5130f42ff77219a6856f8c0
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-28 11:21:33 +0900

    update delete ok

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -29,12 +29,30 @@ const getTasks = async (req,res) => {
     }
 }
 
-const updateTasks = (req,res) => {
-    res.send('updated task')
+const updateTasks = async (req,res) => {
+    try{
+        const {id: taskID} = req.params
+        const task = await Task.findOneAndUpdate({_id: taskID});
+        if(!task){
+            return res.status(404).json({msg: `No task with id : ${taskID}`})
+        }
+        res.status(200).json({task})
+    }catch(error){
+        res.status(500).json({msg: error})
+    }
 }
 
-const deleteTasks = (req,res) => {
-    res.send('deleted task')
+const deleteTasks = async (req,res) => {
+    try{
+        const {id: taskID} = req.params
+        const task = await Task.findOneAndDelete({_id: taskID});
+        if(!task){
+            return res.status(404).json({msg: `No task with id : ${taskID}`})
+        }
+        res.status(200).json({task})
+    }catch(error){
+        res.status(500).json({msg: error})
+    }
 }
 
 
