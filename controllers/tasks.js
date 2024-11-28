da4f564a2a56acaf929bced18c029c402263778f
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-28 11:02:47 +0900

    ok

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -20,9 +20,9 @@ const getTasks = async (req,res) => {
     try{
         const {id: taskID} = req.params
         const task = await Task.findOne({_id: taskID});
-    if(!task){
-        return res.status(404).json({msg: `No task with id : ${taskID}`})
-    }
+        if(!task){
+            return res.status(404).json({msg: `No task with id : ${taskID}`})
+        }
         res.status(200).json({task})
     }catch(error){
         res.status(500).json({msg: error})
